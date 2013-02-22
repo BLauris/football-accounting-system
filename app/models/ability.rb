2574afe5d4936a32aa50d60efb694f40618db62b
Lauris <lauris@lauris-Aspire-5742G.(none)>
2013-02-22 18:05:15 +0200

    fixed ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
       can :manage, Coach, :user_id => user.id
       can :manage, Player, :user_id => user.id
       can :manage, User, :id => user.id
-      can :read, Tournament, :user_id => user.id
+      can :read, Tournament
       can :manage, Staff, :user_id => user.id
     elsif user.is_admin?
       can :administrate
