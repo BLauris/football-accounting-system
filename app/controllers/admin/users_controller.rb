dab28b2777fe62081b2844e3967f0f642047c863
Lauris <lauris@lauris-Aspire-5742G.(none)>
2013-02-23 15:06:51 +0200

    fixed users controller bug in show action

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,37 +8,9 @@ class Admin::UsersController < Admin::AdministratorController
 
   def show
     @user = User.find(params[:id])
-    @coach = Coach.all
-    @staff = Staff.all
-    @player = Player.all
-  end
-
-  def edit
-    @user = User.find(params[:id])
-    if request.xhr?
-      render :text =>  @user.to_json
-    end
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(params[:user])
-      if request.xhr?
-        render :text => {succsess:true}.to_json
-      else
-        flash[:notice] = "Successfully updated"
-        redirect_to :action => "index"
-      end
-    else
-      render :action => "new"
-    end
-  end
-
-  def destroy
-    if User.find(params[:id]).destroy
-      flash[:notice] = "Successfully deleted"
-      redirect_to :action => "index"
-    end
+    @coach = @user.coaches
+    @staff = @user.staffs
+    @player = @user.players
   end
 
 end
